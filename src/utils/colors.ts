05be178cdc5ae33761e01a486c00361987540b0e
nevo993 <nevix399@gmail.com>
2025-07-21 13:20:18 +0200

    Create colors.ts

---
+++ src/utils/colors.ts
@@ -0,0 +1,11 @@
+const distinctColors = [
+  "#e6194b", "#3cb44b", "#ffe119", "#4363d8", "#f58231",
+  "#911eb4", "#46f0f0", "#f032e6", "#bcf60c", "#fabebe",
+  "#008080", "#e6beff", "#9a6324", "#fffac8", "#800000",
+  "#aaffc3", "#808000", "#ffd8b1", "#000075", "#808080"
+];
+
+export function getRandomColor(used: string[] = []) {
+  const available = distinctColors.filter(c => !used.includes(c));
+  return available[Math.floor(Math.random() * available.length)] || "#000000";
+}
